17666882329463aba014d85e1ab6d4e3d960c57a
IanRestrepo <ianyrestrepo@gmail.com>
2024-10-06 02:54:07 -0500

    POST | Logo Slider - About Us Section - Our Projects Section | Finished & Responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-		  animation: {
-			'up-down': 'upDown 2s ease-in-out infinite',
-		  },
-		  keyframes: {
-			upDown: {
-			  '0%, 100%': { transform: 'translateY(0)' },
-			  '50%': { transform: 'translateY(-10px)' },
-			},
-		  },
-		},
-	  },
-	plugins: [],
-}
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        "up-down": "upDown 2s ease-in-out infinite", // Up-down animation
+        'loop-scroll': 'loop-scroll 50s linear infinite',
+      },
+      keyframes: {
+        upDown: {
+          "0%, 100%": { transform: "translateY(0)" },
+          "50%": { transform: "translateY(-10px)" },
+        },
+        'loop-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      },
+    },
+  },
+  plugins: [],
+};
